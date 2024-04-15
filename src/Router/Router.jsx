4fc73fa83895f8db0d889f837cf89e5562405ed1
TimiMaliki <malikitimi@gmail.com>
2024-04-15 13:16:55 +0100

    added:added margin

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "../mainLayout/MainLayout";
+import AuthLayout from "../AuthLayout/Auth";
 import Home from "../pages/Home";
-import Search, { Dogs } from "../pages/Dogs";
+import { Dogs } from "../pages/Dogs";
 import SingleDog from "../pages/SingleDog";
-import SignIn from "../pages/SignIn"
+import SignIn from "../pages/SignIn";
 import NotFound from "../pages/NotFound";
+import LogIn from "../ AuthPages/LogIn";
+import SignUP from "../ AuthPages/SignUP";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="" element={<MainLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/sign" element={<SignIn/>} />
-        <Route path="/dogs" element={<Dogs/>} />
-        <Route path="/dogs/:name" element={<SingleDog/>} />
-        <Route path="*" element={<NotFound/>} />
+        <Route path="/sign" element={<SignIn />} />
+        <Route path="/dogs" element={<Dogs />} />
+        <Route path="/dogs/:name" element={<SingleDog />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
+
+      <Route path="" element={<AuthLayout />}>
+        <Route path="/LogIn" element={<LogIn />} />
+        <Route path="/SignUp" element={<SignUP />} />
+      </Route>
+
     </Routes>
   );
 };
